30e9843df7c144891e6d7306842a30404a2a9b49
He He <hhe@Hes-MBP-16.lan>
2021-12-06 19:44:50 +0800

    just a start

---
+++ chapter1/exercise1-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = -20;
+    upper = 150;
+    step = 10;
+
+    celsius = lower;
+    printf("cels\tfahr\n");
+    while(celsius <= upper){
+        fahr = (9.0/5.0) * celsius + 32;
+        printf("%4.0f %7.1f\n", celsius, fahr);
+        celsius += step;
+    }
+    return 0;
+}
