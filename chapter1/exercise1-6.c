525e5c15898e287d05a8550e04393ba2957673a8
seraphalo <hhe@Hes-MacBook-Pro-16.local>
2022-03-17 23:01:26 +0800

    including the missing ones

---
+++ chapter1/exercise1-6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Verify that the expression getchar() != EOF is 0 or 1. */
+int main(){
+
+	printf("%d\n", getchar() != EOF);
+
+	return 0;
+
+}
---
+++ chapter1/exercise1-7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Write a program to print the value of EOF */
+int main(){
+
+	printf("%d\n", EOF);
+	
+	return 0;
+
+}
