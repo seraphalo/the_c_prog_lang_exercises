579ed4be620555c8caebd15e7f68fa2a3222e003
Mac He <hehezeno@gmail.com>
2025-10-22 19:48:56 +0800

    good

---
+++ chapter1/exercise1-16.c
@@ -0,0 +1,78 @@
+/**
+ * Revise the main routine of the longest-line program so
+ * it will correctly print the length of arbitrarily long lines,
+ * and as much as possible of the text.
+ */
+
+#include <stdio.h>
+
+#define MAXLINE 1000		/* maximum input line size */
+
+/* getline function already exists in the stdio.h*/
+int my_getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
+int main(int argc, char const *argv[])
+{
+	int len;			/* current line length */
+	int max;			/* maximum length seen so far */
+	char line[MAXLINE];		/* current input line */
+	char longest[MAXLINE];	/* longest line saved here */
+	int TBC = 0;
+	int total_len = 0;
+	char line_header[MAXLINE];
+
+	max = 0;
+	while ((len = my_getline(line, MAXLINE)) > 0) {
+		if (len == MAXLINE - 1 && line[MAXLINE - 2] != '\n') {
+			if (TBC == 0) {
+				copy(line_header, line);
+			}
+			total_len += len;
+			TBC = 1;
+			continue;
+		}
+		else if (TBC == 1) {
+			TBC = 0;
+			total_len += len;
+			if (total_len > max) {
+				max = total_len;
+				copy(longest, line_header);
+			}
+			total_len = 0;
+			continue;
+		}
+
+
+		if (len > max) {
+			max = len;
+			copy(longest, line);
+		}
+	}
+	if (max > 0)	/* there was a line */
+		printf("%s\n", longest);
+	return 0;
+}
+
+/* getline: read a line into s, return length */
+int my_getline(char s[], int lim) {
+	int c,i;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[]) {
+	int i;
+	i = 0;
+	while ((to[i] = from[i]) != '\0')
+		++i;
+}
